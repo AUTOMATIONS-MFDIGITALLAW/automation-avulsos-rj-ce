f60ab5010b99121d013740a8d3247ceeb955abc2
JerffesonFlorencio <jerffeson.florencio93@gmail.com>
2025-07-28 10:43:06 -0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,10 +3,10 @@ REM Definir o diretório do projeto
 cd "%USERPROFILE%\Documents\automation-avulsos-rj-ce"
 
 REM Ativar o ambiente virtual
-call .venv\Scripts\activate
+REM call .venv\Scripts\activate
 
 REM Executar o script Python
-python app.py
+start cmd /k "cd %cd% && .venv\Scripts\activate && python monitor.py"
 
 REM Pausar a janela para visualizar mensagens
 pause
